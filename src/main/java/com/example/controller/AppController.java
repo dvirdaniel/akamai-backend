6ed955a8006d64a7c5d8d5126faa99bc2351a3a2
dvirdaniel <dvirdaniel@gmail.com>
2023-09-26 02:28:36 +0300

    Add files via upload

--- src/main/java/com/example/controller/AppController.java
+++ src/main/java/com/example/controller/AppController.java
@@ -1,11 +1,11 @@
 package com.example.controller;
 
+import com.example.model.FileResponse;
 import com.example.service.JsonFileReaderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,8 +14,13 @@ public class AppController {
     @Autowired
     private JsonFileReaderService jsonFileReaderService;
 
-    @GetMapping("/files")
-    Object getFiles(@RequestParam(value = "q", required = false) String prefix) {
+    @GetMapping(path="/files", produces = "application/json")
+    List<FileResponse> getFiles(@RequestParam(value = "q", required = false) String prefix) {
         return this.jsonFileReaderService.getTreeData(prefix);
+    }
+
+    @GetMapping(path="/hello", produces = "application/json")
+    public String sayHello() {
+        return "Hello, World!";
     }
 }
--- src/test/java/com/example/AppControllerTest.java
+++ src/test/java/com/example/AppControllerTest.java
@@ -1,11 +1,64 @@
 package com.example;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.example.controller.AppController;
+import com.example.model.FileResponse;
+import com.example.service.JsonFileReaderService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = App.class)
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AppController.class)
 public class AppControllerTest {
 
+    private MockMvc mockMvc;
+
+    @Mock
+    private JsonFileReaderService jsonFileReaderService;
+
+    @InjectMocks
+    private AppController appController;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(appController).build();
+    }
+
+    @Test
+    public void testGetFilesWithPrefix() throws Exception {
+        String prefix = "filters";
+        List<FileResponse> responseList = new ArrayList<>();
+        when(jsonFileReaderService.getTreeData(prefix)).thenReturn(responseList);
+        mockMvc.perform(get("/files")
+                        .param("q", prefix)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(responseList.toString()));
+    }
+
+    @Test
+    public void testGetFilesWithoutPrefix() throws Exception {
+        List<FileResponse> responseList = new ArrayList<>();
+        when(jsonFileReaderService.getTreeData(null)).thenReturn(responseList);
+        mockMvc.perform(get("/files")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(responseList.toString()));
+    }
 }
